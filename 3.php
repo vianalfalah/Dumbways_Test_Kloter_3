62e948d7f157dda1f422517c99a48ee35db92edc
Vian Alfalah <59907686+vianalfalah@users.noreply.github.com>
2020-10-10 21:45:52 +0700

    Add files via upload

---
+++ 3.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>No. 1</title>
+
+</head>
+<body>
+<h1>For dan While membentuk segitiga siku-siku Prima</h1>
+
+
+<?php
+
+for ($i=2; $i <=9 ; $i++) {
+  $j=2;
+  while ($j <= $i)  {
+    $j++;
+    echo $i;
+  }
+  echo "<br/>";
+}
+ ?>
+</body>
+</html>
